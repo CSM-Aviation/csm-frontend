28b9656ebbae6df05fa360af413abf4b7a622f66
Goutham <gouthamkallem@gmail.com>
2024-08-22 22:48:40 -0700

    ClientLayoutContent error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ClientLayoutContent from "./components/ClientLayoutContent";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react"
