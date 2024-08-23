8532f03a84280035104a2d02442f390c259f6afb
Goutham <gouthamkallem@gmail.com>
2024-08-23 16:14:45 -0700

    Header changes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -47,26 +47,26 @@ const Header: React.FC<HeaderProps> = ({ headerColor }) => {
         className="relative group"
         onMouseEnter={() => handleDropdownHover('charter')}
       >
-        <Link href="/charter" className={`block p-1.5 text-xl font-bold uppercase transition-all duration-300 ${activeDropdown === 'charter' ? 'text-blue-600 underline' : 'text-gray-800 hover:text-blue-600'}`}>CHARTER</Link>
+        <Link href="/charter" className={`block p-1.5 text-xl font-bold uppercase transition-all duration-300 ${activeDropdown === 'charter' ? 'text-blue-600 underline' : 'text-white hover:text-blue-600'}`}>CHARTER</Link>
       </li>
       <li>
-        <Link href="/management" className="block p-1.5 text-xl font-bold text-gray-800 uppercase transition-all duration-300 hover:text-blue-600">MANAGEMENT</Link>
+        <Link href="/management" className="block p-1.5 text-xl font-bold text-white uppercase transition-all duration-300 hover:text-blue-600">MANAGEMENT</Link>
       </li>
       <li>
-        <Link href="/maintenance" className="block p-1.5 text-xl font-bold text-gray-800 uppercase transition-all duration-300 hover:text-blue-600">MAINTENANCE</Link>
+        <Link href="/maintenance" className="block p-1.5 text-xl font-bold text-white uppercase transition-all duration-300 hover:text-blue-600">MAINTENANCE</Link>
       </li>
       <li 
         className="relative group"
         onMouseEnter={() => handleDropdownHover('company')}
       >
-        <Link href="/company" className={`block p-1.5 text-xl font-bold uppercase transition-all duration-300 ${activeDropdown === 'company' ? 'text-blue-600 underline' : 'text-gray-800 hover:text-blue-600'}`}>COMPANY</Link>
+        <Link href="/company" className={`block p-1.5 text-xl font-bold uppercase transition-all duration-300 ${activeDropdown === 'company' ? 'text-blue-600 underline' : 'text-white hover:text-blue-600'}`}>COMPANY</Link>
       </li>
     </>
   );
 
   return (
-    <header ref={headerRef} className="relative w-full z-[1000]">
-      <nav className="relative w-full transition-transform duration-500 shadow-none" style={{ backgroundColor: headerColor }}>
+    <header ref={headerRef} className="absolute top-0 left-0 w-full z-50">
+      {/* <nav className="relative w-full transition-transform duration-500 shadow-none "> */}
         <div className="container mx-auto flex justify-between items-center px-5 py-4">
           <Link href="/" className="flex items-center">
             <Image src="/images/CSM-Logo-WHITE-01-web300.jpg" alt="CSM Aviation" width={80} height={80} className="invert" />
@@ -95,28 +95,28 @@ const Header: React.FC<HeaderProps> = ({ headerColor }) => {
             </button>
           </div>
         </div>
-      </nav>
+      {/* </nav> */}
       
       {/* Full-width Dropdown Menu */}
       {activeDropdown && (
         <div 
           ref={dropdownRef}
-          className="absolute left-0 w-full bg-[#bdae7a] py-8 hidden md:block"
+          className="absolute left-0 w-full py-8 hidden md:block"
           onMouseLeave={handleDropdownLeave}
         >
           <div className="container mx-auto flex justify-center">
             <div className="flex flex-col items-center space-y-4">
               {activeDropdown === 'charter' && (
                 <>
-                  <Link href="/charter/quote" className="text-xl text-gray-800 hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>INSTANT QUOTE</Link>
-                  <Link href="/charter/trip" className="text-xl text-gray-800 hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>TRIP REQUEST</Link>
-                  <Link href="/charter/fleet" className="text-xl text-gray-800 hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>FLEET</Link>
+                  <Link href="/charter/quote" className="text-xl text-gray-800 text-white hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>INSTANT QUOTE</Link>
+                  <Link href="/charter/trip" className="text-xl text-gray-800  text-white hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>TRIP REQUEST</Link>
+                  <Link href="/charter/fleet" className="text-xl text-gray-800 text-white hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>FLEET</Link>
                 </>
               )}
               {activeDropdown === 'company' && (
                 <>
-                  <Link href="/company/about" className="text-xl text-gray-800 hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>ABOUT US</Link>
-                  <Link href="/company/contact" className="text-xl text-gray-800 hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>CONTACT</Link>
+                  <Link href="/company/about" className="text-xl text-gray-800   text-white hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>ABOUT US</Link>
+                  <Link href="/company/contact" className="text-xl text-gray-800 text-white hover:text-white transition-colors duration-300" onClick={() => setActiveDropdown(null)}>CONTACT</Link>
                 </>
               )}
             </div>
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -19,25 +19,24 @@ const Hero: React.FC<HeroProps> = ({ backgroundImage, videoSource, title, subtit
         </video>
       ) : (
         <div 
-          className="absolute top-0 left-0 w-full h-full bg-cover bg-center flex justify-center items-center text-white"
+          className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
           style={{ 
             backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backgroundImage})` 
           }}
-        >
-          <div className="relative z-10 text-center p-5 max-w-4xl">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">{title}</h1>
-            <p className="text-xl md:text-2xl mb-8">{subtitle}</p>
-            {isHome && (
-              <button className="bg-white text-black px-6 py-3 rounded-full font-bold flex items-center justify-center hover:bg-gray-200 transition-colors duration-300">
-                <span>SCHEDULE TRIP</span>
-                <svg className="ml-2 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
-                </svg>
-              </button>
-            )}
-          </div>
-        </div>
+        ></div>
       )}
+      <div className="relative z-10 flex flex-col justify-center items-center h-full text-white pt-20">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">{title}</h1>
+        <p className="text-xl md:text-2xl mb-8">{subtitle}</p>
+        {isHome && (
+          <button className="bg-white text-black px-6 py-3 rounded-full font-bold flex items-center justify-center hover:bg-gray-200 transition-colors duration-300">
+            <span>SCHEDULE TRIP</span>
+            <svg className="ml-2 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
+            </svg>
+          </button>
+        )}
+      </div>
     </section>
   );
 };
