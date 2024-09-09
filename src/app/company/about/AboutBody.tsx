bee339a6f560a9e5224b0c803a15f3c1be4d8def
anmol <anmolsingh2199@gmail.com>
2024-09-09 16:41:58 -0700

    setup

--- src/app/company/about/AboutBody.tsx
+++ src/app/company/about/AboutBody.tsx
@@ -6,6 +6,7 @@ const AboutUsComponent = () => {
     <div className="w-full ">
       {/* Dedicated People Section */}
       <section className=" py-16">
+        
         <div className="container mx-auto px-4">
           <h2 className="text-4xl font-bold text-center mb-8 text-blue-900">Dedicated People Dedicated to Private Aviation</h2>
           <div className="max-w-4xl mx-auto bg-gray-100 p-8 rounded-lg shadow-lg">
