047260b30a2083a9f80e32d4ddbb4003e26d29d3
Goutham <gouthamkallem@gmail.com>
2024-10-31 16:34:52 -0700

    SEO and sitemap changes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     // Fetch dynamic routes from your API or database
     const fleetResponse = await apiService.fetchFleet();
     const fleetPages = fleetResponse.data ? fleetResponse.data.map(item => ({
-        url: `https://www.thisisatestspacefor.design/charter/fleet/${item.registration}+'?model='+${item.aircraftName}`,
+        url: `https://www.csmaviation.com/charter/fleet/${item.registration}+'?model='+${item.aircraftName}`,
         lastModified: new Date(),
         changeFrequency: 'monthly' as const,
         priority: 0.7
@@ -22,7 +22,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         '/company/about',
         '/company/contact'
     ].map((route) => ({
-        url: `https://www.thisisatestspacefor.design${route}`,
+        url: `https://www.csmaviation.com${route}`,
         lastModified: new Date(),
         changeFrequency: 'daily' as const,
         priority: 0.8
@@ -30,7 +30,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     return [
         {
-            url: 'https://www.thisisatestspacefor.design',
+            url: 'https://www.csmaviation.com',
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 1,
