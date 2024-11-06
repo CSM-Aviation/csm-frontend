9867e0abb5501673b2a9f9ce90e9a668a2862a51
anmol <anmolsingh2199@gmail.com>
2024-11-06 15:39:13 -0800

    removed client testimonial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             <SpeedInsights />
             <AnalyticsTracker />
           </main>
-          <Testimonials />
+          {/* <Testimonials /> */}
           <Footer />
         </ConfigProvider>
       </body>
